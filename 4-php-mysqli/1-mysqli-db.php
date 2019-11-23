887252dab0b95d96c31cf0a2472d39d78ee57c89
otumian <popecan1000@gmail.com>
2019-11-23 21:40:22 +0000

    4-php-mysqli/1-mysqli-db.php

---
+++ 4-php-mysqli/1-mysqli-db.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="style.css"/>
+		<title>Php-Studies</title>
+	</head>
+	<body>
+		<div class="container">
+			<h1>MySQLi DB</h1>
+			<ol>
+				<li>Database</li>
+				<li>SQL standards</li>
+				<li>Tables of rows and cols</li>
+				<li>Queries</li>
+                <li><a href="http://www.mysql.com/">Download MySQLi</a></li>
+			</ol>
+			
+			<?php ?>
+		</div>
+	</body>
+</html>
